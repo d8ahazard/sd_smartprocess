a5dcbc42b63db4a06ea8a63cd7323b0d5a6b7010
d8ahazard <d8ahazard@gmail.com>
2022-12-06 08:22:34 -0600

    Update install.py

--- install.py
+++ install.py
@@ -5,5 +5,5 @@ from launch import run
 
 name = "Smart Crop"
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
-print(f"loading Dreambooth reqs from {req_file}")
+print(f"loading {name} reqs from {req_file}")
 run(f'"{sys.executable}" -m pip install -r "{req_file}"', f"Checking {name} requirements.", f"Couldn't install {name} requirements.")
