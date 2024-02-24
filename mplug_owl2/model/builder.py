34dac6859faab7b5604059708d753d397de0086f
d8ahazard <d8ahazard@gmail.com>
2024-02-24 15:07:17 -0600

    Remove OWLV1

--- mplug_owl2/model/builder.py
+++ mplug_owl2/model/builder.py
@@ -47,7 +47,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
     else:
         kwargs['torch_dtype'] = torch.float16
     if 'mplug_owl2' in model_name.lower():
-        # Load LLaVA model
+        # Load LLaVA2 model
         if 'lora' in model_name.lower() and model_base is None:
             warnings.warn(
                 'There is `lora` in model name but no `model_base` is provided. If you are loading a LoRA model, please provide the `model_base` argument. Detailed instruction: https://github.com/haotian-liu/LLaVA#launch-a-model-worker-lora-weights-unmerged.')
